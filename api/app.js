1ce7804a48567233a53180fb883b37c396aaaafc
Aayush Thapa <aayushthapaa213@gmail.com>
2024-12-25 23:04:13 +0545

    project completed

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
 import productRouter from "./routes/productRouter.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
@@ -11,7 +12,17 @@ app.use("/products", productRouter);
 
 const { PORT } = process.env || 5000;
 
-app.listen( PORT, (err) => {
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, (err) => {
   connectDB();
   if (err) console.log(err);
   console.log(`Server is running on port ${PORT}`);
